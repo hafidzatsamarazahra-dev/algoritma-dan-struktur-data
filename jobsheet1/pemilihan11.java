c49f557bdeedcdd119d1bab21d4186a5ef491878
hafidza <hafidzatsamarazahra@gmail.com>
2026-02-10 10:15:41 +0700

    pembetulan pemilihan 2.1

--- jobsheet1/pemilihan11.java
+++ jobsheet1/pemilihan11.java
@@ -5,6 +5,8 @@ public class pemilihan11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Program Menghitung Nilai Akhir Mahasiswa");
+        System.out.println("===============================");
         System.out.print("Masukkan Nilai Tugas: ");
         double tugas = input.nextDouble();
         System.out.print("Masukkan Nilai Kuis: ");
@@ -13,24 +15,21 @@ public class pemilihan11 {
         double uts = input.nextDouble();
         System.out.print("Masukkan Nilai UAS: ");
         double uas = input.nextDouble();
-
+        
         if (tugas < 0 || tugas > 100 || kuis < 0 || kuis > 100 ||
             uts < 0 || uts > 100 || uas < 0 || uas > 100) {
 
             System.out.println("===================");
-            System.out.println("===================");
             System.out.println("nilai tidak valid");
-            System.out.println("===================");
             System.out.println("===================");
 
         } else {
-            double nilaiAkhir = (tugas + kuis + uts + uas) / 4;
-            System.out.println("===================");
+            double nilaiAkhir = (0.2 * tugas) + (0.2 * kuis) + (0.3 * uts) + (0.4 * uas);
+            
             System.out.println("===================");
             System.out.println("nilai akhir: " + nilaiAkhir);
 
             String huruf;
-
             if (nilaiAkhir > 80) huruf = "A";
             else if (nilaiAkhir > 73) huruf = "B+";
             else if (nilaiAkhir > 65) huruf = "B";
@@ -41,9 +40,8 @@ public class pemilihan11 {
 
             System.out.println("Nilai Huruf: " + huruf);
             System.out.println("===================");
-            System.out.println("===================");
 
-            if (huruf.equals("E"))
+            if (huruf.equals("D") || huruf.equals("E"))
                 System.out.println("ANDA TIDAK LULUS");
             else
                 System.out.println("SELAMAT ANDA LULUS");
