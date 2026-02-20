98a38520f211688a9e9af5f2e7e1a017a8402093
hafidza <hafidzatsamarazahra@gmail.com>
2026-02-20 20:38:14 +0700

    praktikum 2.2.1

--- jobsheet2/mahasiswaMain11.java
+++ jobsheet2/mahasiswaMain11.java
@@ -4,7 +4,7 @@ public class mahasiswaMain11 {
     public static void main(String[] args) {
         mahasiswa11 mhs1 = new mahasiswa11();
        
-        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nama = "Muhammad Ali farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
