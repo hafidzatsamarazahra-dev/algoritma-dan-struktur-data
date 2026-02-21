0861904d8f59694f433514279ede090606a3ccbb
hafidza <hafidzatsamarazahra@gmail.com>
2026-02-21 11:16:02 +0700

    praktikum 2.3.1

--- jobsheet2/mahasiswa11.java
+++ jobsheet2/mahasiswa11.java
@@ -1,52 +1,33 @@
 package jobsheet2;
-public class mahasiswa11 {
 
+public class mahasiswa11 {
     String nim;
     String nama;
     String kelas;
     double ipk;
 
+    public mahasiswa11() {
+    }
+
+    public mahasiswa11(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim; 
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
-        System.out.println("Ipk: " + ipk);
+        System.out.println("IPK: " + ipk);
         System.out.println("Kelas: " + kelas);
     }
-
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
+    
     void updateIpk(double ipkBaru) {
         if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
             ipk = ipkBaru;
         } else {
-            System.out.println("Ipk tidak valid. Harus antara 0.0 dan 4.0");
+            System.out.println("IPK tidak valid.");
         }
-    }
-
-    String nilaiKinerja(double ipk) {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
-        }
-    }
-
-    public static void main(String[] args) {
-        mahasiswa11 mhs = new mahasiswa11();
-
-        mhs.nama = "Hafidza";
-        mhs.nim = "254107020034";
-        mhs.kelas = "TI-1G";
-
-        mhs.updateIpk(5);
-
-        mhs.tampilkanInformasi();
-        System.out.println("Nilai Kinerja: " + mhs.nilaiKinerja(mhs.ipk));
     }
 }
