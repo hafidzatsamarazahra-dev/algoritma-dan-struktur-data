f2b82d10a8a3c317ad5679387e124c8ac7d5cbdb
hafidza <hafidzatsamarazahra@gmail.com>
2026-02-21 11:28:56 +0700

    praktikum 2.3.3 jawaban pertanyaan no 3

--- jobsheet2/mahasiswa11.java
+++ jobsheet2/mahasiswa11.java
@@ -6,12 +6,9 @@ public class mahasiswa11 {
     String kelas;
     double ipk;
 
-    public mahasiswa11() {
-    }
-
     public mahasiswa11(String nm, String nim, double ipk, String kls) {
         nama = nm;
-        this.nim = nim; 
+        this.nim = nim;
         this.ipk = ipk;
         kelas = kls;
     }
@@ -21,13 +18,5 @@ public class mahasiswa11 {
         System.out.println("NIM: " + nim);
         System.out.println("IPK: " + ipk);
         System.out.println("Kelas: " + kelas);
-    }
-    
-    void updateIpk(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-            ipk = ipkBaru;
-        } else {
-            System.out.println("IPK tidak valid.");
-        }
     }
 }
