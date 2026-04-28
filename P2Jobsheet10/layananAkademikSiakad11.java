722692c52f097da2c99682d35a655815a2301186
hafidza <hafidzatsamarazahra@gmail.com>
2026-04-28 09:25:46 +0700

    percobaan 2

---
+++ P2Jobsheet10/layananAkademikSiakad11.java
@@ -0,0 +1,67 @@
+package P2Jobsheet10;
+import java.util.Scanner;
+
+public class layananAkademikSiakad11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        antrianLayanan11 antrian = new antrianLayanan11(5);
+        int pilihan;
+
+        do {
+            System.out.println("\n=== Menu Antrian Layanan Akademik ===");
+            System.out.println("1. Tambah Mahasiswa ke antrian");
+            System.out.println("2. Layani Mahasiswa");
+            System.out.println("3. Lihat Mahasiswa Terdepan");
+            System.out.println("4. Lihat Semua Antrian");
+            System.out.println("5. Jumlah mahasiswa dalam antrian");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+
+            pilihan = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("NIM   : ");
+                    String nim = sc.nextLine();
+                    System.out.print("Nama  : ");
+                    String nama = sc.nextLine();
+                    System.out.print("Prodi : ");
+                    String prodi = sc.nextLine();
+                    System.out.print("Kelas : ");
+                    String kelas = sc.nextLine();
+
+                    mahasiswa11 mhs = new mahasiswa11(nim, nama, prodi, kelas);
+                    antrian.tambahAntrian(mhs);
+                    break;
+
+                case 2:
+                    mahasiswa11 dilayani = antrian.layaniMahasiswa();
+                    if (dilayani != null) {
+                        System.out.print("Melayani mahasiswa:");
+                        dilayani.tampilkanData();
+                    }
+                    break;
+
+                case 3:
+                    antrian.lihatTerdepan();
+                    break;
+
+                case 4:
+                    antrian.tampilkanSemua();
+                    break;
+
+                case 5:
+                    System.out.println("Jumlah dalam antrian: " + antrian.getJumlahAntrian());
+                    break;
+                case 0:
+                    System.out.println("terima kasih.");
+                    break;
+                default:
+                    System.out.println("pilihan tidak valid.");
+            }
+
+        } while (pilihan != 0);
+        sc.close();
+    }
+}
---
+++ P2Jobsheet10/mahasiswa11.java
@@ -0,0 +1,21 @@
+package P2Jobsheet10;
+
+public class mahasiswa11 {
+
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+
+    public mahasiswa11(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData() {
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
+
+}
